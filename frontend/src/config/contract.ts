113a24ead2ca2984afc35dba4cd1faadb74016f9
saajan.varghese.2006@gmail.com <saajan.varghese.2006@gmail.com>
2026-01-05 17:46:21 +0530

    push

--- frontend/src/config/contract.ts
+++ frontend/src/config/contract.ts
@@ -1,3 +1,3 @@
 // Local Hardhat deployment
-export const CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+export const CONTRACT_ADDRESS = "0x73511669fd4de447fed18bb79bafeac93ab7f31f"
 export const CHAIN_ID = 31337
